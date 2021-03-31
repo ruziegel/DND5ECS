9cd69b6b050ba4faf7ecc29329e12aa71c75d213
Ziegel <skvorcov-pa-vel@yandex.ru>
2021-03-31 14:20:40 +0300

    Сделал UI главного окна, добавил немного полей в Создание персонажа

--- data/CharacterModel.py
+++ data/CharacterModel.py
@@ -1,3 +1,10 @@
 class Character:
-    def __init__(self, name):
+    def __init__(self):
+        self.name = None
+        self.race = None
+
+    def set_name(self, name):
         self.name = name
+
+    def set_race(self, race):
+        self.race = race
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import pyqt5_tools
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from data.CharacterSheet import Ui_CreateCharacterWindow
+from data.MainWindow import Ui_MainWindow
+class CreateC(QMainWindow, Ui_CreateCharacterWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.CharacterNameLineEdit.textChanged.connect(self)
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = CreateC()
+    ex.show()
+    sys.exit(app.exec_())
